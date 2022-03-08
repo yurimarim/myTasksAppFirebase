95edff5ee0109d7beed2abd63b84396e197cffe0
yurimarim <yurimarim@hotmail.com>
2022-03-08 15:06:51 -0300

    new: database config, create tasks

--- App.js
+++ App.js
@@ -1,16 +1,56 @@
 import React, { useState } from 'react'
-import { StatusBar } from 'expo-status-bar'
 import {
   StyleSheet,
-  Text,
+  Keyboard,
+  View,
   TextInput,
   TouchableOpacity,
-  SafeAreaView
+  SafeAreaView,
+  FlatList
 } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+import { Feather } from 'react-native-vector-icons'
 import { Login } from './src/components/Login'
+import { TaskList } from './src/components/TaskList'
+import firebase from './src/services/firebaseConnection'
 
 export default function App() {
   const [user, setUser] = useState(null)
+  const [tasks, setTasks] = useState([])
+  const [newTask, setNewTask] = useState('')
+
+  function handleAddTask() {
+    if (newTask === '') {
+      return
+    }
+
+    let tasks = firebase.database().ref('tasks').child(user)
+    let key = tasks.push().key
+
+    tasks
+      .child(key)
+      .set({
+        name: newTask
+      })
+      .then(() => {
+        const data = {
+          key: key,
+          name: newTask
+        }
+
+        setTasks(oldTasks => [...oldTasks, data].reverse())
+      })
+    setNewTask('')
+    Keyboard.dismiss()
+  }
+
+  function handleDeleteTask(id) {
+    console.log(id)
+  }
+
+  function handleEditTask(data) {
+    console.log('ITEM CLICADO ' + JSON.stringify(data))
+  }
 
   if (!user) {
     return <Login changeStatus={user => setUser(user)} />
@@ -19,7 +59,30 @@ export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="dark" />
-      <Text>DENTRO DA TELA TAREFAS!!!</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          value={newTask}
+          onChangeText={text => setNewTask(text)}
+          style={styles.input}
+          placeholder="Adicione sua tarefa..."
+          underlineColorAndroid="transparent"
+        />
+        <TouchableOpacity onPress={handleAddTask} style={styles.buttonAddTask}>
+          <Feather name="plus" size={26} color="#fff" />
+        </TouchableOpacity>
+      </View>
+
+      <FlatList
+        data={tasks}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+          <TaskList
+            data={item}
+            deleteItem={handleDeleteTask}
+            editItem={handleEditTask}
+          />
+        )}
+      />
     </SafeAreaView>
   )
 }
@@ -30,5 +93,31 @@ const styles = StyleSheet.create({
     paddingTop: 25,
     paddingHorizontal: 10,
     backgroundColor: '#f2f6fc'
+  },
+  inputContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: 20
+  },
+  input: {
+    flex: 1,
+    height: 45,
+    backgroundColor: '#fff',
+    borderRadius: 4,
+    fontSize: 16,
+    padding: 10
+  },
+  buttonAddTask: {
+    backgroundColor: '#3ea6f2',
+    paddingHorizontal: 15,
+    paddingVertical: 8,
+    marginLeft: 10,
+    borderRadius: 5
+  },
+  btnText: {
+    color: '#fff',
+    fontSize: 20
   }
 })
---
+++ src/components/TaskList/index.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TouchableWithoutFeedback
+} from 'react-native'
+import { Feather } from '@expo/vector-icons'
+
+export function TaskList({ data, deleteItem, editItem }) {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={() => deleteItem(data.id)}>
+        <Feather name="trash" size={20} color="#fff" />
+      </TouchableOpacity>
+
+      <View style={{ paddingRight: 10 }}>
+        <TouchableWithoutFeedback onPress={() => editItem(data)}>
+          <Text style={{ color: '#fff', marginLeft: 10, paddingRight: 10 }}>
+            {data.name}
+          </Text>
+        </TouchableWithoutFeedback>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginVertical: 10,
+    flexDirection: 'row',
+    backgroundColor: '#121212',
+    alignItems: 'center',
+    padding: 10,
+    borderRadius: 4
+  }
+})
